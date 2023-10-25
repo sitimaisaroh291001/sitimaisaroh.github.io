1f2705d0dfb53bed11992e48d40782fd4b5d30de
sitimaisaroh291001 <148961759+sitimaisaroh291001@users.noreply.github.com>
2023-10-25 21:07:05 +0700

    Add files via upload

--- manage.py
+++ manage.py
@@ -7,14 +7,15 @@ import sys
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'my_saroh.settings')
-    try:
-        in main from django.core.management import execute_from_command_line
+        try:
+            from django.core.management import execute_from_command_line
             except ImportError as exc:
-        in main raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
+
+            raise ImportError(
+                "Couldn't import Django. Are you sure it's installed and "
+                "available on your PYTHONPATH environment variable? Did you "
+                "forget to activate a virtual environment?"
+                ) from exc
 in main
   execute_from_command_line(sys.argv)
 
